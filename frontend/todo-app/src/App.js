450caf7656b79dbda9f5d6606771b949684d6301
kundan <kundan621@gmail.com>
2021-02-27 11:54:58 +0530

    basic counter

--- frontend/todo-app/src/App.js
+++ frontend/todo-app/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 import './App.css';
+import Counter from './components/counter/Counter';
 import  FirstComponent from './components/my-learnings/FirstComponent';
 import { SecondComponent } from './components/my-learnings/SecondComponent';
 import { ThirdComponent } from './components/my-learnings/ThirdComponent';
@@ -9,6 +10,16 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+         <Counter/>
+      </div>
+    );
+  }
+}
+// eslint-disable-next-line no-unused-vars
+class LearningComponent extends Component {
+  render() {
+    return (
+      <div className="LearningComponent">
          Hello world
          <FirstComponent/>
          <SecondComponent/>
---
+++ frontend/todo-app/src/components/counter/Counter.jsx
@@ -0,0 +1,9 @@
+import './counter.scss';
+export default function Counter() {
+    return (
+      <div className="Counter">
+        <button>+1</button>
+        <span className="count">0</span>
+      </div>
+    );
+}
