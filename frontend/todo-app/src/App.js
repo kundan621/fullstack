efcda51a4ba8c9a3e44cdcf62796e7b9742712eb
kundan <kundan621@gmail.com>
2021-02-27 11:30:38 +0530

    components finished

--- frontend/todo-app/src/App.js
+++ frontend/todo-app/src/App.js
@@ -1,25 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
 import './App.css';
+import  FirstComponent from './components/my-learnings/FirstComponent';
+import { SecondComponent } from './components/my-learnings/SecondComponent';
+import { ThirdComponent } from './components/my-learnings/ThirdComponent';
  
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-        </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-        </a>
-        </header>
+         Hello world
+         <FirstComponent/>
+         <SecondComponent/>
+         <ThirdComponent/>
       </div>
     );
   }
---
+++ frontend/todo-app/src/components/my-learnings/ThirdComponent.js
@@ -0,0 +1,7 @@
+export const ThirdComponent= () => {
+    return (
+      <div className="ThirdComponent">
+        Third Component
+      </div>
+    );
+}
